91ebac1bb5e57fde0406a6abf31ad3975b049eb3
Samuel Mueller <muellesa@tf.uni-freiburg.de>
2020-09-23 14:50:25 +0200

    Mini Networks for unittests.

--- RandAugment/networks/__init__.py
+++ RandAugment/networks/__init__.py
@@ -10,6 +10,8 @@ from RandAugment.networks.pyramidnet import PyramidNet
 from RandAugment.networks.shakeshake.shake_resnet import ShakeResNet
 from RandAugment.networks.wideresnet import WideResNet
 from RandAugment.networks.shakeshake.shake_resnext import ShakeResNeXt
+from RandAugment.networks.convnet import SeqConvNet
+from RandAugment.networks.mlp import MLP
 
 from RandAugment.meta_state_adaption import AdaptiveDropouter, Modulator
 
@@ -18,22 +20,26 @@ def get_model(conf, bs, val_bs, optimizer_creator_factory, num_class=10, writer=
     name = conf['type']
     assert not ('adaptive_dropouter' in conf and 'adaptive_modulator' in conf)
     if 'adaptive_dropouter' in conf:
-        assert name in ('wresnet28_10',)
+        assert name in ('wresnet28_10','miniconvnet')
         ad_conf = conf['adaptive_dropouter']
 
         if ad_conf['simple_dropout']:
             ad_creators = (lambda w: torch.nn.Dropout(p=1.-ad_conf['target_p']), lambda w: torch.nn.Dropout(p=1.-ad_conf['target_p']))
         else:
-            ad_creators = (lambda w: AdaptiveDropouter(w, ad_conf['hidden_size'], optimizer_creator_factory(), bs, val_bs, cross_entropy_alpha=ad_conf['cross_entropy_alpha'], target_p=ad_conf['target_p'], out_bias=ad_conf['out_bias'], relu=ad_conf['relu'], inference_dropout=ad_conf.get('inference_dropout', False), scale_by_p=ad_conf.get('scale_by_p', False), summary_writer=writer),
+            ad_creators = (lambda w: AdaptiveDropouter(w, ad_conf['hidden_size'], optimizer_creator_factory(), bs, val_bs, cross_entropy_alpha=ad_conf['cross_entropy_alpha'], target_p=ad_conf['target_p'], out_bias=ad_conf['out_bias'], relu=ad_conf['relu'], inference_dropout=ad_conf.get('inference_dropout', False), scale_by_p=ad_conf.get('scale_by_p', False), batch_norm=ad_conf['batch_norm'], ppo=ad_conf['ppo'], tanh=ad_conf['tanh'], summary_writer=writer).cuda(),
                            lambda planes, kernel_size, stride, padding: AdaptiveDropouter((planes, kernel_size, stride, padding), ad_conf['hidden_size'], optimizer_creator_factory(), bs,
                                                        val_bs, cross_entropy_alpha=ad_conf['cross_entropy_alpha'],
                                                        target_p=ad_conf['target_p'], out_bias=ad_conf['out_bias'],
                                                        relu=ad_conf['relu'],
                                                        inference_dropout=ad_conf.get('inference_dropout', False),
                                                        scale_by_p=ad_conf.get('scale_by_p', False),
-                                                       summary_writer=writer))
+                                                       batch_norm=ad_conf['batch_norm'],
+                                                       summary_writer=writer).cuda())
         if not ad_conf.get('conv_dropout', False):
             ad_creators = (ad_creators[0], None)
+        else:
+            assert not ad_conf['ppo']
+            assert not ad_conf['tanh']
     elif 'adaptive_modulator' in conf:
         assert name in ('wresnet28_10',)
         ad_conf = conf['adaptive_modulator']
@@ -51,6 +57,10 @@ def get_model(conf, bs, val_bs, optimizer_creator_factory, num_class=10, writer=
         model = WideResNet(40, 2, dropout_rate=0.0, num_classes=num_class, adaptive_dropouter_creator=ad_creators[0],adaptive_conv_dropouter_creator=ad_creators[1])
     elif name == 'wresnet28_10':
         model = WideResNet(28, 10, dropout_rate=0.0, num_classes=num_class, adaptive_dropouter_creator=ad_creators[0],adaptive_conv_dropouter_creator=ad_creators[1])
+    elif name == 'miniconvnet':
+        model = SeqConvNet(num_class,adaptive_dropout_creator=ad_creators[0],batch_norm=False)
+    elif name == 'mlp':
+        model = MLP(num_class, (3,32,32))
     elif name == 'shakeshake26_2x32d':
         model = ShakeResNet(26, 32, num_class)
     elif name == 'shakeshake26_2x64d':
@@ -77,6 +87,8 @@ def get_model(conf, bs, val_bs, optimizer_creator_factory, num_class=10, writer=
 def num_class(dataset):
     return {
         'cifar10': 10,
+        'noised_cifar10': 10,
+        'targetnoised_cifar10': 10,
         'reduced_cifar10': 10,
         'cifar10.1': 10,
         'cifar100': 100,
