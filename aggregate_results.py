2631bdf8cd628032745b4fc3b5442c8095b93878
Samuel Mueller <muellesa@tf.uni-freiburg.de>
2020-10-27 11:30:18 +0100

    Update Log View Scripts

---
+++ aggregate_results.py
@@ -0,0 +1,52 @@
+from tensorboard.backend.event_processing import event_accumulator
+from os import listdir
+from os.path import isfile, join
+import re
+import argparse
+import numpy as np, scipy.stats as st
+def get_last_top1(path):
+    onlyfiles = sorted([join(path,f) for f in listdir(path) if isfile(join(path, f))])
+    last_point = 0
+    v = None
+    for f in onlyfiles:
+        ea = event_accumulator.EventAccumulator(f)
+        # top1 not found
+        ea.Reload()
+        try:
+            e = ea.Scalars('top1')[-1]
+            if e.step >= last_point:
+                if last_point > 0:
+                    print("Warning: Multiple runs with one name:", f, "other result:", v, 'at', last_point)
+                last_point = e.step
+                v = e.value
+        except Exception as e:
+            print(e)
+    return v, last_point
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get Top1 Results.')
+    parser.add_argument('path',
+                   help='Prefix of the paths/conf files for the runs to evaluate.')
+    parser.add_argument('--logdir', default='logs3')
+    args = parser.parse_args()
+    mypath = args.path #'wresnet28x10_cifar100_4xb64_valsteps_maxlr.1_learnedrandprepreprocessorensemble_optwidelongsesp_50epochs_2_.0ent_no0augs_.001mlr_exploresm'
+    suffix = '.yaml'
+    if mypath.endswith(suffix):
+        mypath = mypath[:-len(suffix)]
+    logdir = args.logdir
+    paths = [path for path in listdir(logdir) if re.search(f'{mypath}(_.try|).yaml',path)]
+    print([path[len(mypath):] for path in paths])
+
+    paths = [join(join(logdir,path),'test') for path in paths]
+    results = [get_last_top1(path) for path in paths]
+    assert all([r[1] == results[0][1] for r in results]), results
+    step = results[0][1]
+    results = [r[0] for r in results]
+    assert all(results), results
+    print(f"The results are the following {len(results)} at step {step}: {results}")
+
+    results = np.array(results)
+    n = len(results)
+    m, se = np.mean(results), st.sem(results)
+    confidence = .95
+    h = se * st.t.ppf((1 + confidence) / 2., n-1)
+    print(f"Mean: {round(np.mean(results),4)}, Std: {round(np.std(results),4)}, +/-: {round(h,4)}")
---
+++ average_results.py
@@ -0,0 +1,73 @@
+from tensorboard.backend.event_processing import event_accumulator
+from tensorboardX import SummaryWriter
+from os import listdir
+from os.path import isfile, join
+import re
+import argparse
+from collections import defaultdict
+import numpy as np, scipy.stats as st
+from tqdm import tqdm
+def get_last_curve(path, tag='top1'):
+    onlyfiles = sorted([join(path,f) for f in listdir(path) if isfile(join(path, f))])
+    last_point = 0
+    v = None
+    for f in onlyfiles:
+        ea = event_accumulator.EventAccumulator(f)
+        # top1 not found
+        ea.Reload()
+        try:
+            e = ea.Scalars(tag)[-1]
+            if e.step >= last_point:
+                if last_point > 0:
+                    print("Warning: Multiple runs with one name:", f, "other result:", v, 'at', last_point)
+                last_point = e.step
+                v = ea.Scalars(tag)
+        except Exception as e:
+            print(e)
+    return v, last_point
+
+def get_exp_name(path: str):
+    path = path.split('/')[-1]
+    return re.sub(r"(_[0-9]+try)?.yaml","", path)
+
+def curves_to_matrix(curves):
+    matrix = np.zeros((len(curves), min(len(c) for c in curves)))
+    for t,events in enumerate(zip(*curves)):
+        assert all(e.step == events[0].step for e in events), [e.step for e in events]
+        matrix[:,t] = np.array([e.value for e in events])
+    return matrix
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get Top1 Results.')
+    parser.add_argument('--logdir', default='logs3')
+    parser.add_argument('--out_logdir', default='logs3averaged')
+    args = parser.parse_args()
+
+    directories = [join(args.logdir,d) for d in listdir(args.logdir)]
+    directory_map = defaultdict(list)
+    for d in directories:
+        directory_map[get_exp_name(d)].append(d)
+    for exp_name, exp_instance_directory in tqdm(directory_map.items()):
+        for split in ['test']:
+            ds = [join(d,split) for d in exp_instance_directory]
+            for tag in ['top1',]:
+                curves = [get_last_curve(d, tag)[0] for d in ds]
+                curves = [c for c in curves if c is not None]
+                if not curves:
+                    continue
+                matrix = curves_to_matrix(curves)
+                means = matrix.mean(axis=0)
+                out_writer = SummaryWriter(log_dir=join(args.out_logdir,exp_name+f'_average_of_{len(curves)}'))
+                for step, mean in zip([e.step for e in curves[0]], means):
+                    out_writer.add_scalar(tag+'_'+split, mean, step)
+
+
+
+
+
+
+
+
